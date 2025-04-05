dc3b0b9897df17828a96d144b4d30e08c42e447d
Uttam Sharma <Uttamsharma1711@gmail.com>
2025-04-06 02:32:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/*const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const connectDB = require('./src/database/connection'); // Import the database connection
 const messageHandler = require('./src/events/message');// Adjust the path as necessary
-const guildMemberAddHandler = require('./src/events/guildMemberAdd');
 const readyHandler = require('./src/events/ready');
 
 // Load environment variables from .env file
@@ -21,9 +19,6 @@ const client = new Client({
 });
 
 
-// Connect to MongoDB
-connectDB(); // Call the function to connect to the database
-
 // Event handlers
 messageHandler(client);
 guildMemberAddHandler(client);
@@ -33,47 +28,3 @@ readyHandler(client);
 client.login(process.env.DISCORD_TOKEN)
     .then(() => console.log('Bot logged in successfully'))
     .catch(err => console.error('Failed to log in:', err));
- */
-    const { Client, GatewayIntentBits, Collection } = require('discord.js');
-    const mongoose = require('mongoose');
-    const dotenv = require('dotenv');
-    const connectDB = require('./src/database/connection'); // Import the database connection
-    const miscHandler = require('./src/events/misc'); // Updated path
-    const prefixCommandsHandler = require('./src/events/prefix'); // Updated path
-    const slashCommandsHandler = require('./src/events/slash'); // Updated path
-    const guildMemberAddHandler = require('./src/events/guildMemberAdd');
-    const readyHandler = require('./src/events/ready');
-    const imagesenderHandler = require('./src/events/imagesender');
-    const keepAlive = require('./server');
-    
-
-    // Load environment variables from .env file
-    dotenv.config();
-    
-    // Create a new Discord client with required intents
-    const client = new Client({
-        intents: [
-            GatewayIntentBits.Guilds,
-            GatewayIntentBits.GuildMessages,
-            GatewayIntentBits.MessageContent,
-            GatewayIntentBits.GuildMembers,
-            GatewayIntentBits.GuildPresences
-        ],
-    });
-    
-    // Connect to MongoDB
-    connectDB(); // Call the function to connect to the database
-    keepAlive();
-    // Event handlers
-    miscHandler(client);
-    prefixCommandsHandler(client);
-    slashCommandsHandler(client);
-    guildMemberAddHandler(client);
-    //imagesenderHandler(client);
-    readyHandler(client);
-    
-    // Log in to Discord
-    client.login(process.env.DISCORD_TOKEN)
-        .then(() => console.log('Bot logged in successfully'))
-        .catch(err => console.error('Failed to log in:', err));
-    
